25400d3ea83c91a8f62af0641b14957db9dffbd4
Xyphuz <hansen0715.2@gmail.com>
2022-11-19 21:48:20 +0800

    chore: add length table with generator

---
+++ src/util/length_table_generator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::cout << "static uint8_t length_table[256] = {" << std::endl;
+
+    for (int fourth = 1; fourth <= 4; fourth++) {
+        for (int third = 1; third <= 4; third++) {
+            for (int second = 1; second <= 4; second++) {
+                for (int first = 1; first <= 4; first++) {
+                    std::cout << "\t" << first + second + third + fourth << ", //" << first << second << third << fourth << std::endl;
+                }
+            }
+        }
+    }
+
+    std::cout << "};" << std::endl;
+
+    return 0;
+}
