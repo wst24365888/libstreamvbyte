cd53139279c5923012ad7e4b0658b833d7967ec6
Xyphuz <hansen0715.2@gmail.com>
2022-12-05 22:26:27 +0800

    fix: __builtin_expect is not compatible with msvc

--- libstreamvbyte/branch_prediction.h
+++ libstreamvbyte/branch_prediction.h
@@ -6,9 +6,17 @@
 // https://cibuildwheel.readthedocs.io/en/stable/cpp_standards/
 
 #ifndef LIKELY
-#define LIKELY(x) __builtin_expect(!!(x), 1)
+    #if defined(__GNUC__) || defined(__clang__)
+        #define LIKELY(x) __builtin_expect(!!(x), 1)
+    #else
+        #define LIKELY(x) (x)
+    #endif
 #endif
 
 #ifndef UNLIKELY
-#define UNLIKELY(x) __builtin_expect(!!(x), 0)
+    #if defined(__GNUC__) || defined(__clang__)
+        #define UNLIKELY(x) __builtin_expect(!!(x), 0)
+    #else
+        #define UNLIKELY(x) (x)
+    #endif
 #endif
