64d337176ddf5735a7390547a1e89a5fb67e39db
Xyphuz <hansen0715.2@gmail.com>
2022-12-11 02:00:11 +0800

    feat: bind zigzag into python

--- src/streamvbyte.cpp
+++ src/streamvbyte.cpp
@@ -24,7 +24,7 @@ PYBIND11_MODULE(libstreamvbyte, m) {
 
             return out;
         },
-        "Encode a list of integers into a byte array.");
+        "Encode an array of unsigned integers into a byte array.");
 
     m.def(
         "decode", [](py::array_t<uint8_t> in, std::size_t size) {
@@ -41,5 +41,39 @@ PYBIND11_MODULE(libstreamvbyte, m) {
 
             return out;
         },
-        "Decode a byte array into a list of integers.");
+        "Decode a byte array into an array of unsigned integers.");
+
+    m.def(
+        "encode_zigzag", [](py::array_t<int32_t> in) {
+            py::buffer_info in_buf = in.request();
+
+            if (in_buf.ndim != 1) {
+                throw std::runtime_error("Number of dimensions must be one.");
+            }
+
+            auto out = py::array_t<uint32_t>(in_buf.size);
+            py::buffer_info out_buf = out.request();
+
+            streamvbyte::encode_zigzag(reinterpret_cast<int32_t*>(in_buf.ptr), in_buf.size, reinterpret_cast<uint32_t*>(out_buf.ptr));
+
+            return out;
+        },
+        "Encode an array of signed integers into an array of unsigned integers.");
+
+    m.def(
+        "decode_zigzag", [](py::array_t<uint32_t> in) {
+            py::buffer_info in_buf = in.request();
+
+            if (in_buf.ndim != 1) {
+                throw std::runtime_error("Number of dimensions must be one.");
+            }
+
+            auto out = py::array_t<int32_t>(in_buf.size);
+            py::buffer_info out_buf = out.request();
+
+            streamvbyte::decode_zigzag(reinterpret_cast<uint32_t*>(in_buf.ptr), in_buf.size, reinterpret_cast<int32_t*>(out_buf.ptr));
+
+            return out;
+        },
+        "Decode an array of unsigned integers into an array of signed integers.");
 }
--- tests/test.py
+++ tests/test.py
@@ -2,12 +2,10 @@ import libstreamvbyte as svb
 import numpy as np
 
 N = 2**20 + 2
-LOW = 0
-HIGH = 2**32
 
 
-def test_encode_decode():
-    before_encode = np.random.randint(LOW, HIGH, N, dtype=np.uint32)
+def test_streamvbyte():
+    before_encode = np.random.randint(0, 2**32, N, dtype=np.uint32)
 
     compressed_bytes = svb.encode(before_encode)
     assert type(compressed_bytes) == np.ndarray
@@ -16,6 +14,20 @@ def test_encode_decode():
     after_decode = svb.decode(compressed_bytes, len(before_encode))
     assert type(after_decode) == np.ndarray
     assert after_decode.dtype == np.uint32
+
+    assert len(before_encode) == len(after_decode)
+    assert np.array_equal(before_encode, after_decode)
+
+def test_zigzag():
+    before_encode = np.random.randint(-2**31, 2**31, N, dtype=np.int32)
+
+    after_encode = svb.encode_zigzag(before_encode)
+    assert type(after_encode) == np.ndarray
+    assert after_encode.dtype == np.uint32
+
+    after_decode = svb.decode_zigzag(after_encode)
+    assert type(after_decode) == np.ndarray
+    assert after_decode.dtype == np.int32
 
     assert len(before_encode) == len(after_decode)
     assert np.array_equal(before_encode, after_decode)
