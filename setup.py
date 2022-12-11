3822f22c6dc38383196aa204a2b4396511eb4f3e
Xyphuz <hansen0715.2@gmail.com>
2022-12-11 20:35:13 +0800

    chore: update tag

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='libstreamvbyte',
-    version='0.2.2',
+    version='0.2.3',
     description='A C++ implementation with Python bindings of StreamVByte.',
     long_description=open('README.md').read(),
     author='HSING-HAN WU (Xyphuz)',
