9e6e24de4f48cea28bd26cc49437d6b1f077a045
Xyphuz <hansen0715.2@gmail.com>
2022-12-12 16:55:04 +0800

    chore: add throughput in benchmark

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -1,5 +1,5 @@
-#include <benchmark/benchmark.h>
 #include "streamvbyte.h"
+#include <benchmark/benchmark.h>
 
 static void BM_streamvbyte_encode(benchmark::State& state) {
     std::size_t N = state.range(0);
@@ -14,6 +14,8 @@ static void BM_streamvbyte_encode(benchmark::State& state) {
     for (auto _ : state) {
         streamvbyte::encode(original_data, N, compressed_bytes);
     }
+
+    state.counters["Throughput"] = benchmark::Counter(int64_t(state.iterations()) * int64_t(N) * int64_t(sizeof(uint32_t)), benchmark::Counter::kIsRate);
 
     delete[] original_data;
     delete[] compressed_bytes;
@@ -36,6 +38,8 @@ static void BM_streamvbyte_decode(benchmark::State& state) {
         streamvbyte::decode(compressed_bytes, recovered_data, N);
     }
 
+    state.counters["Throughput"] = benchmark::Counter(int64_t(state.iterations()) * int64_t(N) * int64_t(sizeof(uint32_t)), benchmark::Counter::kIsRate);
+
     delete[] original_data;
     delete[] compressed_bytes;
     delete[] recovered_data;
@@ -54,6 +58,8 @@ static void BM_streamvbyte_encode_zigzag(benchmark::State& state) {
     for (auto _ : state) {
         streamvbyte::encode_zigzag(original_data, N, encoded_unsigend_integers);
     }
+
+    state.counters["Throughput"] = benchmark::Counter(int64_t(state.iterations()) * int64_t(N) * int64_t(sizeof(uint32_t)), benchmark::Counter::kIsRate);
 
     delete[] original_data;
     delete[] encoded_unsigend_integers;
@@ -76,18 +82,16 @@ static void BM_streamvbyte_decode_zigzag(benchmark::State& state) {
         streamvbyte::decode_zigzag(encoded_unsigend_integers, N, recovered_data);
     }
 
+    state.counters["Throughput"] = benchmark::Counter(int64_t(state.iterations()) * int64_t(N) * int64_t(sizeof(uint32_t)), benchmark::Counter::kIsRate);
+
     delete[] original_data;
     delete[] encoded_unsigend_integers;
     delete[] recovered_data;
 }
 
-BENCHMARK(BM_streamvbyte_encode)->Range(1 << 20, 1 << 20);
-BENCHMARK(BM_streamvbyte_encode)->Range(1 << 30, 1 << 30)->Iterations(100);
-BENCHMARK(BM_streamvbyte_decode)->Range(1 << 20, 1 << 20);
-BENCHMARK(BM_streamvbyte_decode)->Range(1 << 30, 1 << 30)->Iterations(100);
-BENCHMARK(BM_streamvbyte_encode_zigzag)->Range(1 << 20, 1 << 20);
-BENCHMARK(BM_streamvbyte_encode_zigzag)->Range(1 << 30, 1 << 30)->Iterations(100);
-BENCHMARK(BM_streamvbyte_decode_zigzag)->Range(1 << 20, 1 << 20);
-BENCHMARK(BM_streamvbyte_decode_zigzag)->Range(1 << 30, 1 << 30)->Iterations(100);
+BENCHMARK(BM_streamvbyte_encode)->RangeMultiplier(10)->Range(1e6, 1e9)->MinTime(10);
+BENCHMARK(BM_streamvbyte_decode)->RangeMultiplier(10)->Range(1e6, 1e9)->MinTime(10);
+BENCHMARK(BM_streamvbyte_encode_zigzag)->RangeMultiplier(10)->Range(1e6, 1e9)->MinTime(10);
+BENCHMARK(BM_streamvbyte_decode_zigzag)->RangeMultiplier(10)->Range(1e6, 1e9)->MinTime(10);
 
 BENCHMARK_MAIN();
